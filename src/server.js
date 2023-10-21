352873f887c555baa59cc888d97b6d27a0c1fa3a
Carmina Familar <99389599+CarminaF@users.noreply.github.com>
2023-10-21 10:31:37 +1030

    added request body message

--- src/server.js
+++ src/server.js
@@ -5,10 +5,20 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 //const HOST = '0.0.0.0';
 
+app.use(express.json());
+
+// Config above
+// Routes below
 app.get("/", (request, response) => {
     response.json({
         message: "Hello World!"
     })
+})
+
+app.post("/", (request, response) => {
+    response.json({
+        received: request.body.message
+    });
 })
 
 module.exports = {
